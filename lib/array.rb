53916933d776806e16a2dcc0a37fcb3fd7ed9c53
Michael <Cleary481@gmail.com>
2013-12-10 12:28:25 +0000

    sorting array

--- lib/array.rb
+++ lib/array.rb
@@ -3,9 +3,21 @@ class Array
   def insertion_sort
     sorted = [self.shift]
     unsorted = self
-
-    # Do Stuff
-
+    unsorted_item = unsorted[0]
+    
+    while unsorted_item      
+      if unsorted_item >= sorted[-1]
+        sorted << unsorted.shift
+      else
+        sorted.each_with_index do |s, i|
+          if s > unsorted_item
+            sorted.insert(i, unsorted.shift)
+            break
+          end
+        end
+      end
+      unsorted_item = unsorted[0]
+    end
     sorted
   end
 
